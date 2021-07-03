ff3c14819c0a2814a6d2c247d91d79ae1f85f601
Wangxq <3024881272@qq.com>
2021-07-03 18:59:25 +0800

    heap Data structure

--- 数据结构/heap.js
+++ 数据结构/heap.js
@@ -5,42 +5,130 @@
 
 
 /**
- * 从最后一个叶子节点的父节点 n/2 开始，从上往下堆化。 
- * @param {*} nums 
- * @param {*} heapSize 
+ *  建立一个大顶堆 时间复杂度： O(logn)，为树的高度
+ * 
+ * 1.  插入节点  从下往上堆化  大顶堆
+ * 因为节点是从最后面开始插入的
  */
-function buildHeap(nums, heapSize) {
-    for (let i = Math.floor(heapSize / 2); i >= 1; i--) {
-        heapify(nums, heapSize, i); //堆化
+
+function Heap() {
+    let items = [, ]
+
+    for (let i = 0; i < 7; i++) insert(i + 1)
+        // 从最后一个节点的父元素开始比较  子元素大于父元素 就交换
+    function insert(key) {
+        items.push(key);
+        let index = items.length - 1;
+        while (Math.floor(index / 2) > 0 && items[index] > items[Math.floor(index / 2)]) {
+            swap(items, index, Math.floor(index / 2))
+                // 从儿子到父亲
+            index = Math.floor(index / 2);
+        }
     }
+
+    function swap(items, i, j) {
+        [items[i], items[j]] = [items[j], items[i]]
+    }
+    console.log(items) // [ <1 empty item>, 7, 4, 6, 1, 3, 2, 5 ]
 }
+
+// Heap();
 
 /**
- * 分别比较父元素和左右孩子，如果子元素较小，就和 父元素交换。父元素交换之后，向下依次比较，确保下面的结构都是父元素小于左右孩子
- * @param {*} nums 
- * @param {*} heapSize 
- * @param {*} i 
+ * 2. 原地建堆 已经有数组元素  小顶堆
+ * 
  */
-function heapify(nums, heapSize, i) {
-    let minIndex = i; // 记录最小元素的下标
-    if (i * 2 <= heapSize && nums[i * 2] < nums[i]) minIndex = i * 2;
-    if (i * 2 + 1 <= heapSize && nums[i * 2 + 1] < nums[minIndex]) minIndex = i * 2 + 1;
-    if (minIndex !== i) { //需要交换
-        swap(nums, i, minIndex);
-        //向下调整
-        heapify(nums, heapSize, minIndex);
+let arr = [, 5, 2, 3, 4, 1]
+    /**
+     * 
+     * 2.1  从前往后、自下而上式堆化建堆从第二排开始 和父元素比较 
+     */
+function buildHeap(items, heapSize) {
+    // heapSize 超出数组长度
+    while (heapSize < items.length - 1) {
+        //  不需要处理下标为 1 的数
+        heapSize++;
+        heapify(items, heapSize)
     }
-
 }
 
-function swap(num, i, j) {
-    const temp = num[i];
-    num[i] = num[j];
-    num[j] = temp;
+function heapify(items, i) {
+    // 自下而上式堆化 如果比父元素小 就交换
+    while (Math.floor(i / 2) > 0 && items[i] < items[Math.floor(i / 2)]) {
+        console.log(i)
+        swap(items, i, Math.floor(i / 2))
+            // 从儿子到父亲 自下而上
+        i = Math.floor(i / 2);
+    }
 }
 
-// test
-// 用完全二叉树来存储 第一个节点不存储元素
-const arr = [, 5, 1, 1, 2, 0, 0];
-buildHeap(arr, arr.length - 1)
-console.log(arr); //[ <1 empty item>, 1, 2, 3, 4, 5 ]
+
+// buildHeap(arr, 1);
+// console.log(arr)  // [ <1 empty item>, 9, 8, 7, 6, 3, 2, 4, 1, 5 ]
+
+// 2.2 从后往前、自上而下式堆化建堆
+// 从最后一个非叶子节点开始, 从 n/2 位置节点开始堆化
+
+function buildHeap1(items, heapSize) {
+    // 从 n/2 位置节点开始堆化  因为到了最后一个元素是要向上和每一个父元素比较
+    for (let i = Math.floor(heapSize / 2); i >= 1; --i) {
+        heapify1(items, heapSize, i)
+    }
+}
+
+function heapify1(items, heapSize, i) {
+    // 自上而下式堆化
+    while (true) {
+        let minIndex = i;
+        //  大顶堆 找出当前元素最大的孩子 进行交换
+        if (2 * i <= heapSize && items[i] < items[i * 2]) {
+            minIndex = i * 2;
+        }
+        if (2 * i + 1 <= heapSize && items[minIndex] < items[i * 2 + 1]) {
+            minIndex = i * 2 + 1;
+        }
+        // 父亲已经是最小的了
+        if (minIndex === i) break;
+        swap(items, i, minIndex);
+        i = minIndex; // 从父亲到儿子  自上而下
+    }
+}
+
+function swap(items, i, j) {
+
+    [items[i], items[j]] = [items[j], items[i]]
+}
+// var item = [,5, 2, 3, 4, 1]
+
+//  length -1 ,第一个元素不算，从下标 1  开始
+// buildHeap1(item, item.length - 1)
+// console.log(item) //[ <1 empty item>, 1, 2, 3, 4, 5 ]
+
+
+/**
+ * 排序算法：堆排序  顺序
+ * 取堆的第一个元素（最大值） 和 最后一个元素交换
+ * 
+ * 
+ */
+
+function heapSort(items) {
+    // 构建大顶堆
+    buildHeap1(items, items.length - 1)
+        // 防止数组越界
+    let haepSize = items.length - 1;
+    for (let i = items.length - 1; i > 1; i--) {
+        // 最后一个元素和第一个元素交换
+        swap(items, 1, i);
+        // 有序长度减 1
+        haepSize--;
+        // 堆化有序序列
+        //  自上而下式堆化
+        heapify1(items, haepSize, 1)
+    }
+    return items;
+}
+
+var items = [, 1, 9, 2, 8, 3, 7, 4, 6, 5]
+heapSort(items)
+console.log(items);
